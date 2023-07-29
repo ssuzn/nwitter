c413406c3b60cc724ef1db13e61a9c2742f2b200
ssuzn <tnwls_723@naver.com>
2023-07-29 23:35:27 +0900

    delete, edit button

---
+++ src/components/Nweet.js
@@ -0,0 +1,64 @@
+import { dbService } from "fbase";
+import React, { useState } from "react";
+
+const Nweet = ({ nweetObj, isOwner }) => {
+  // Edit 버튼
+  const [editing, setEditing] = useState(false); // editing mode 인가?
+  const [newNweet, setNewNweet] = useState(nweetObj.text); // input에 입력된 text update
+  
+  // delete 버튼
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`nweets/${nweetObj.id}`).update({
+      text: newNweet,
+    });
+    setEditing(false); // editing mode 해제
+  };
+  // edit 내용 변경 중일때 내용 표시
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewNweet(value);
+    };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text" 
+              placeholder="Edit your nweet"
+              value={newNweet} 
+              required 
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && ( // owner일 때만 button 보임 
+            <>
+              <button onClick={onDeleteClick}>Delete</button>
+              <button onClick={toggleEditing}>Edit</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Nweet;
